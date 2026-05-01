34089464f8432771ec9ba234a3e005f2662efff8
kaissy-a <kaissyamaro2@gmail.com>
2026-04-30 23:27:19 -0700

    Added navigation between, dashboard and new game.

--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -61,8 +61,17 @@ public class SceneFactory {
             Button pastScores = new Button("View Past Scores: ");
 
             newGame.setOnAction(e -> {
-                System.out.println("New Game!");
+                stage.setScene(SceneFactory.create(SceneType.GAME, stage));
             });
+
+            pastScores.setOnAction( e -> {
+                stage.setScene(SceneFactory.create(SceneType.LEADERBOARD, stage));
+
+            });
+            leaderboard.setOnAction(e->{
+                stage.setScene(SceneFactory.create(SceneType.LEADERBOARD, stage));
+            });
+
             VBox layout = new VBox();
             layout.getChildren().addAll(title, newGame, leaderboard, pastScores);
             return new Scene(layout, 600, 400);
