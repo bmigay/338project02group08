18ce5f90f041d7f2a56a76ce0a7e6be6be965902
kaissy-a <kaissyamaro2@gmail.com>
2026-05-05 21:48:44 -0700

    added filepath

---
+++ src/main/java/com/daclink/SceneType.java
@@ -0,0 +1,17 @@
+package com.daclink;
+/**
+ * [Description of what the class does]
+ *
+ * @version 0.1.0
+ * @author: Braeden Migay
+ * @since: 4/20/26
+ */
+public enum SceneType {
+    LOGIN,
+    DASHBOARD,
+    GAME,
+    LEADERBOARD,
+    NEWUSER,
+    CATEGORIES,
+    PASTSCORES
+}
